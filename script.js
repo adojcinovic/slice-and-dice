4c5b6e043940ba70593a31812433a9469a27f459
adojcinovic <adojca@gmail.com>
2021-11-01 13:21:31 +0100

    almost done

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-var buttons = document.querySelectorAll("button");
-var quotes = document.querySelector(".quotes");
+var buttons = document.querySelectorAll(".dots i");
+var details = document.querySelector(".author-quote");
 var author = document.createElement("p");
-var quote = document.createElement("p");
+var quote = document.createElement("h4");
+console.log(buttons);
+console.log(details);
 
 var obj = [
-  { quote: "tralalala", author: "pera peric" },
-  { quote: "gneg", author: "zika peric" },
-  { quote: "tralnonooalala", author: "mika peric" },
-  { quote: "piowphwn", author: "zmika peric" },
-  { quote: "wphwn", author: "mitar skoro" },
+  {
+    quote:
+      '"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt vero voluptas blanditiis nostrum non at aspernatur doloråofficia"',
+    author: "SUZAN SIMS, INTERACTION DESIGNER AD XYZ",
+  },
+  {
+    quote:
+      '"Amet maiores sit, recusandae totam est corporis repellendus. Deserunt quam unde optio magnam reprehenderit voluptateaccusamus."',
+    author: "SAM SMITH, CEO PHARMACY DIGITAL",
+  },
+  {
+    quote:
+      '"Est praesentium ipsam sint quae iste earum natus delectus omnis quam unde optio magnam reprehenderit voluptateaccusamus"',
+    author: "MIKE MALONE, HEAD OF TRAINING, DNGTS",
+  },
+  {
+    quote:
+      '"Consectetur, eveniet ullam laudantium eos dolore in explicabo alias consequatur agnam reprehenderit voluptateaccusamu."',
+    author: "GEORGE BROWN, CTO TRALALA",
+  },
+  {
+    quote:
+      '"Facere ex deleniti molestias earum non minus adipisci laboriosam fugit? Blanditiis a dolorem ad fuga facere provident dicta perferendis molestias."',
+    author: "MITAR SKORO, HOLYCODE",
+  },
 ];
-author.innerHTML = obj[0].author;
-quotes.appendChild(author);
 quote.innerHTML = obj[0].quote;
-quotes.appendChild(quote);
+details.appendChild(quote);
+author.innerHTML = obj[0].author;
+details.appendChild(author);
 console.log(buttons);
 
 buttons.forEach((element, index) => {
